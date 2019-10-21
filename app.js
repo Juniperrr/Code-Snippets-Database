de1512961e9c70f5b97f0bc9a8190e46022d0cb6
Juniperrr <sohnjunhee2011@gmail.com>
2019-10-21 03:37:38 -0400

    added app.js and layout.hbs

--- app.js
+++ app.js
@@ -22,15 +22,42 @@ app.use(function(req, res, next) {
 });
 
 app.get('/', function(req, res) {
-  res.render('home', {snippets});
+  filtered = [];
+  // if there is a filter, then filter = empty array
+  // if there is no filter, filter = all of this.
+  for (i = 0; i < snippets.length; i++) {
+    if ((snippets[i].hasTag(req.query.tagQ) || req.query.tagQ == '') &&
+        snippets[i].lines >= req.query.lineQ &&
+        snippets[i].code.includes(req.query.textQ)) {
+      filtered.push(snippets[i]);
+    }
+  }
+  if (filtered.length === 0) {
+    filtered = snippets;
+  }
+  // filtered = snippets
+  res.render('home', {snippets: filtered});
 });
-app.get('/add', function(req, res) {
+
+app.get('/add', (req, res) => {
   res.render('add');
 });
 
-// app.listen(3000);
-// console.log('Server started; type CTRL+C to shut down ');
-
+app.post('/add', function(req, res) { // req: server receives
+  console.log('app.post/add req.body', req.body);
+  const name = req.body.name;
+  const code = req.body.code;
+  const tag = req.body.tag;
+  const s = new snippet.Snippet(
+      name,
+      code,
+      tag,
+  );
+  console.log('s', s);
+  snippets.push(s);
+  console.log('snippets.length', snippets.length);
+  res.render('add'); // server: return following
+});
 const s = new snippet.Snippet(
     'hello.js',
     `const hello = () => {
@@ -43,8 +70,9 @@ const s = new snippet.Snippet(
 console.log(s.name); // hello.js
 console.log(s.code); // the entirety of the code
 console.log(s.lines); // 4 (this needs to be calculated!)
-console.log(s.tags); // an Array, ['hello', 'function', 'arrow']
+console.log(s.tags); // an Array, ['hello', 'function', 'arrow'] z
 console.log(s.hasTag('fun')); // false
+// TODO: make it return true
 console.log(s.hasTag('function')); // true
 
 // read all files, save snips in snippets, print out the entire snppets.
